c17fbd2bcb70b1b99167dbf2c4aa0c691dfeb2cc
Colin Tolliver <colintolliver@gmail.com>
2023-12-10 10:18:05 -0800

    React Tic Tac Toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
-import "./App.css";
 import Board from "./Components/Board.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
 function App() {
   return <Board />;
 }
 
 export default App;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
